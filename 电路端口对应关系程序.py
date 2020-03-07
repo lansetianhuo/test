5c4793d6e2e485ecf4b42aa308fefe980045e2ce
lansetianhuo <24279100@qq.com>
2020-03-08 00:15:35 +0800

    优化端口对应关系程序

--- 电路端口对应关系程序.py
+++ 电路端口对应关系程序.py
@@ -2,13 +2,14 @@ import re
 # 导入IP处理模块
 from IPy import IP
 
-local_ip_path = r"D:\xuexi\cheshiwenjian\本端及IP信息.txt"
-remote_ip_path = r"D:\xuexi\cheshiwenjian\对端及IP信息.txt"
+local_ip_path = r"D:\xuexi\cheshiwenjian\本端ip.txt"
+remote_ip_path = r"D:\xuexi\cheshiwenjian\14.17.txt"
 
 with open(local_ip_path, 'r', encoding='utf-8-sig') as file_local_ip:
-    local_data = file_local_ip.read()
+    local_data = file_local_ip.readlines()
 with open(remote_ip_path, 'r', encoding='utf-8-sig') as file_remote_ip:
-    remote_data = file_remote_ip.read()
+    remote_data = file_remote_ip.readlines()
+# print(remote_data)
 
 
 # 判断字符串是否为IP函数
@@ -23,26 +24,18 @@ def ip_true(port_ip):
 
 # 文件输入端口对应IP数据格式化函数
 def intface_format(intf_data):
-    intf_data = intf_data.strip(' ')
-    intf_data = intf_data.strip('\n')
     intf_list = []
-    intf_data_list = re.split('\n', intf_data)
-    for intf in intf_data_list:
-        intf = intf.strip(' ')
-        intf = intf.strip('\t')
-        intf = intf.replace('\t', ' ')
-        for col_data in range(intf.find('  ')):
-            intf = intf.replace('  ', ' ')
-            col_data = range(intf.find('  '))
-        intf = re.split(r'[ ]', intf)
-        intf = intf[:2] + re.split('/', str(intf[2]))
+    for intf in intf_data:
+        intf = intf.split()
+        intf = intf[:2] + intf[2].split('/')
         if ip_true(intf[2]):
             intf_list.append(intf)
     return intf_list
 
 
-local_intf_list = intface_format(local_data)
-remote_intf_list = intface_format(remote_data)
+local_intf_list = intface_format(local_data.copy())
+
+remote_intf_list = intface_format(remote_data.copy())
 
 # 两台设备端口IP匹配函数
 intf_to_intf_data = []
