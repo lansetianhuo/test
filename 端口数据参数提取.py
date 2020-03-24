a738cfbb285b3d6d0c1f8bedf7faef0b15e56ee8
lansetianhuo <24279100@qq.com>
2020-03-24 18:49:00 +0800

    静态绑定用户数据提取

--- 端口数据参数提取.py
+++ 端口数据参数提取.py
@@ -1,6 +1,38 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+"""
+@文件     :   端口数据参数提取
+@时间     :   2020-3-24 11:46
+@作者     :   lansetianhuo
+@版本     :   1.0
+@联系方式 :   24279100@qq.com
+@功能     :
+"""
+# Start typing your code from here
 import re
 import shelve
 import csv
+
+
+def read_data(data_file, data_name):
+    data = shelve.open(data_file)
+    original_data = data[data_name]
+    data.close()
+    return original_data
+
+
+def write_data(file_name, data_name, data):
+    stud_db = shelve.open(file_name)
+    stud_db[data_name] = data
+    stud_db.close()
+
+
+def write_csv(file_name, list):
+    headers = list[0].keys()
+    with open(file_name, 'w', encoding='utf-8-sig', newline='') as file:
+        file_dict = csv.DictWriter(file, headers)
+        file_dict.writeheader()
+        file_dict.writerows(list)
 
 
 def inft_data_collect(intf_data_set):
@@ -17,7 +49,8 @@ def inft_data_collect(intf_data_set):
         shutdown = 'none'
         vpn_name = 'none'
         traffic_policy = 'none'
-        vpls = 'none'
+        pw = 'none'
+        vpls_name = 'none'
         mtu = 'none'
         isis_router = 'none'
         isis_v6_router = 'none'
@@ -58,6 +91,11 @@ def inft_data_collect(intf_data_set):
                 qinq_vlan = re.findall(
                     r'qinq termination pe-vid (\d+) ce-vid (\d+)', data_row)
                 vlan_list += qinq_vlan
+            if data_row.startswith('vlan-type dot1q'):
+                dot1q_vlan = re.findall(r'vlan-type dot1q (\d+)', data_row)
+                dot1q_vlan += ['']
+                dot1q_vlan_tuple = tuple(dot1q_vlan)
+                vlan_list.append(dot1q_vlan_tuple)
             if data_row.startswith('ip address'):
                 ip_address = re.findall(r'ip address (.+)', data_row)
                 ip_address_list += ip_address
@@ -80,7 +118,7 @@ def inft_data_collect(intf_data_set):
             if data_row.startswith('traffic-policy'):
                 traffic_policy = data_row.replace('traffic-policy ', '')
             if data_row.startswith('mpls l2vc'):
-                vpls = re.findall(r'mpls l2vc ((?:\d+.){3}\d+) (\d+)', data_row)[0]
+                pw = re.findall(r'mpls l2vc ((?:\d+.){3}\d+) (\d+)', data_row)[0]
             if data_row.startswith('mtu'):
                 mtu = data_row.replace('mtu ', '')
             if data_row.startswith('isis enable'):
@@ -97,6 +135,8 @@ def inft_data_collect(intf_data_set):
                 isis_cost = re.findall(r'isis cost (\d+) ?', data_row)[0]
             if data_row.startswith('isis small-hello'):
                 isis_small_hello = data_row.replace('isis ', '')
+            if data_row.startswith('l2 binding vsi'):
+                vpls_name = data_row.replace('l2 binding vsi ', '')
 
         intf_data_dict['intf_name'] = intf_name
         intf_data_dict['description'] = description
@@ -110,7 +150,7 @@ def inft_data_collect(intf_data_set):
         intf_data_dict['shutdown'] = shutdown
         intf_data_dict['vpn_name'] = vpn_name
         intf_data_dict['traffic_policy'] = traffic_policy
-        intf_data_dict['vpls'] = vpls
+        intf_data_dict['pw'] = pw
         intf_data_dict['mtu'] = mtu
         intf_data_dict['isis_router'] = mtu
         intf_data_dict['isis_router'] = isis_router
@@ -120,39 +160,21 @@ def inft_data_collect(intf_data_set):
         intf_data_dict['isis_v6_cost'] = isis_v6_cost
         intf_data_dict['isis_cost'] = isis_cost
         intf_data_dict['isis_small_hello'] = isis_small_hello
+        intf_data_dict['vpls_name'] = vpls_name
         if intf_data_dict['isis_router'] != 'none' or intf_data_dict['isis_v6_router'] != 'none':
             intf_data_dict['inft_type'] = 'router_intf'
         elif len(intf_data_dict['ip_address_list']) > 0 or len(intf_data_dict['ipv6_address_list']) > 0:
             intf_data_dict['inft_type'] = 'gateway_intf'
         elif intf_data_dict['domain_name'] != 'none':
             intf_data_dict['inft_type'] = 'domain_intf'
-        elif intf_data_dict['vpls'] != 'none':
+        elif intf_data_dict['pw'] != 'none':
+            intf_data_dict['inft_type'] = 'pw_intf'
+        elif intf_data_dict['vpls_name'] != 'none':
             intf_data_dict['inft_type'] = 'vpls_intf'
         else:
             intf_data_dict['inft_type'] = 'other_intf'
         inft_data_dict_list.append(intf_data_dict)
     return inft_data_dict_list
-
-
-def read_data(data_file, data_name):
-    data = shelve.open(data_file)
-    original_data = data[data_name]
-    data.close()
-    return original_data
-
-
-def write_data(file_name, data_name, data):
-    stud_db = shelve.open(file_name)
-    stud_db[data_name] = data
-    stud_db.close()
-
-
-def write_csv(file_name, list):
-    headers = list[0].keys()
-    with open(file_name, 'w', encoding='utf-8-sig', newline='') as file:
-        file_dict = csv.DictWriter(file, headers)
-        file_dict.writeheader()
-        file_dict.writerows(list)
 
 
 def main():
